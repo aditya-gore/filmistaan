3eb63a0f169ad32e61b2b07b9cb5a5c2dff7f5a0
aditya gore <aditya_gore@hotmail.com>
2023-10-11 17:41:31 +0530

    fetch real genres from db

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,11 +7,16 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
-    //* get movies by [Type]
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+
+    //* get Movies by [Type]
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
   }),
 });
 
-export const { useGetMoviesQuery } = tmdbApi;
+export const { useGetGenresQuery, useGetMoviesQuery } = tmdbApi;
