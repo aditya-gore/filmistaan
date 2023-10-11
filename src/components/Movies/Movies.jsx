6797d866a8073eaa7e8a768f803a8f719cfa0678
aditya gore <aditya_gore@hotmail.com>
2023-10-11 17:10:38 +0530

    movie list view done

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-cycle */
 /* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
 import {
@@ -9,10 +12,34 @@ import {
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  return <div>Movies</div>;
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress size='4rem' />
+      </Box>
+    );
+  }
+  if (!data.results.length) {
+    return (
+      <Box display='flex' alignItems='center' mt='20px'>
+        <Typography variant='h4'>
+          No matching movies found.
+          <br />
+          Please search fore something else
+        </Typography>
+      </Box>
+    );
+  }
+  if (error) return 'An error has occurred';
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
